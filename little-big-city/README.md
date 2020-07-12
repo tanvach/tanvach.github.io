7f9eb2fa687e9df239a179b2ba38a1d381b33178
Tan Vachiramon <tanvach@fb.com>
2020-07-12 14:46:31 -0700

    remove little big city

--- little-big-city/README.md
+++
@@ -1,1 +0,0 @@
-![](./asset/screenshot.png)
--- little-big-city/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    entry: __dirname + '/src/main.js',
-    devtool: 'sourcemap',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    }
-};
